15d4c529f74be25c72109bd711bfa60f8873d8f1
pvvx <pvvx@mail.ru>
2017-02-03 21:54:11 +0300

    update

--- development/rtl87xx/tools/JLink-RunRAM.bat
+++ development/rtl87xx/tools/JLink-RunRAM.bat
@@ -9,9 +9,17 @@ echo w4 0x40000210,0x20011117>>%1\RunRAM.JLinkScript
 echo g>>%1\RunRAM.JLinkScript
 echo q>>%1\RunRAM.JLinkScript
 rem PATH=D:\MCU\SEGGER\JLink_V610a;%PATH%;
-%2\JLink.exe -Device CORTEX-M3 -If SWD -Speed 4000 %1\RunRAM.JLinkScript >null
+%2\JLink.exe -Device CORTEX-M3 -If SWD -Speed 3500 %1\RunRAM.JLinkScript >null
 goto end_x
 :no_dir
 rem PATH=D:\MCU\SEGGER\JLink_V610a;%PATH%;
-start JLink.exe -Device CORTEX-M3 -If SWD -Speed 4000 RunRAM.JLinkScript
+echo h>RunRAM.JLinkScript
+echo r>>RunRAM.JLinkScript
+echo loadbin %LOCALAPPDATA%\Arduino15\packages\realtek\tools\ameba_tools\1.0.8\bsp\image\ram_1.r.bin 0x10000bc8>>RunRAM.JLinkScript
+echo loadbin %LOCALAPPDATA%\Arduino15\packages\realtek\tools\ameba_tools\1.0.8\ram_2.bin 0x10006000>>RunRAM.JLinkScript
+echo r>>RunRAM.JLinkScript
+echo w4 0x40000210,0x20011117>>RunRAM.JLinkScript
+echo g>>RunRAM.JLinkScript
+echo q>>RunRAM.JLinkScript
+start JLink.exe -Device CORTEX-M3 -If SWD -Speed 3500 RunRAM.JLinkScript
 :end_x
--- development/rtl87xx/tools/JLinkGDB_OTA.bat
+++ development/rtl87xx/tools/JLinkGDB_OTA.bat
@@ -1,4 +1,5 @@
 @echo off
+if %1x==x goto no_dir
 if not exist %1\ota.bin goto error
 PATH=%1;%2;%PATH%
 if exist %2\ota.bin del %2\ota.bin
@@ -6,6 +7,12 @@ copy /b %1\ota.bin %2\ota.bin
 cd %2
 start JLinkGDBServer.exe -device Cortex-M3 -if SWD -ir -endian little -speed 3500 
 %3arm-none-eabi-gdb.exe -x gdb_ota.jlink
+taskkill /F /IM JLinkGDBServer.exe
+goto end
+:no_dir
+PATH=D:\MCU\SEGGER\JLink_V612i;%LOCALAPPDATA%\Arduino15\packages\realtek\tools\ameba_tools\arm-none-eabi-gcc\4.8.3-2014q1\bin;%PATH% 
+start JLinkGDBServer.exe -device Cortex-M3 -if SWD -ir -endian little -speed 3500 
+arm-none-eabi-gdb.exe -x gdb_ota.jlink
 taskkill /F /IM JLinkGDBServer.exe
 goto end
 :error
