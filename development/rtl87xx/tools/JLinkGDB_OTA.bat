9bbc461925007641586a749072dd64b92bf729d7
pvvx <pvvx@mail.ru>
2017-02-14 04:59:42 +0300

    update

--- development/rtl87xx/tools/JLinkGDB_OTA.bat
+++ development/rtl87xx/tools/JLinkGDB_OTA.bat
@@ -1,12 +1,12 @@
 @echo off
 if %1x==x goto no_dir
-if not exist "%1\ota.bin" goto error_x
+if not exist %1\ota.bin goto error_x
 PATH=%1;%2;%PATH%
-if exist "%2\ota.bin" del "%2\ota.bin"
-copy /b "%1\ota.bin" "%2\ota.bin"
+if exist %2\ota.bin" del %2\ota.bin
+copy /b %1\ota.bin %2\ota.bin
 cd "%2"
 start JLinkGDBServer.exe -device Cortex-M3 -if SWD -ir -endian little -speed 3500 
-"%3arm-none-eabi-gdb.exe -x gdb_ota.jlink"
+%3arm-none-eabi-gdb.exe -x gdb_ota.jlink
 taskkill /F /IM JLinkGDBServer.exe
 goto end
 :no_dir
